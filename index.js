f2c5dd17af77dc1406fb874c4356fb2d0db3435c
Aayush sharma <aayushsharma@Aayushs-MacBook-Air.local>
2025-04-21 23:52:52 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
 let mongoose=require("mongoose")
 let express=require("express")
 let db2=require("./model/model2.js")
+let fn=async()=>{
+    for(let i=0;i<203;i++){
+       let room={
+        roomno:i+1
+       }
+       await db2.create(room);
+
+}
+}
 let fun=async()=>{
 // const url=mongodb+srv://nodeuser:<db_password>@cluster0.ag3fwai.mongodb.net/
 mongoose.connect("mongodb+srv://nodeuser:Yahoo123@cluster0.ag3fwai.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 let db=mongoose.connection;
+fn();
 db.once('open',()=>{
 console.log("database connected");
 })
 db.on('error',()=>{
     console.log("data base coneection error");
 })
-}
-fun();
-let fn=async()=>{
-    for(let i=0;i<203;i++){
-       let room={
-        roomno:i+1
-       }
-       db2.insertOne(room);
-
-}
-}
-fn();
 let app=express();
 app.use(express.json())
 app.use(express.json());
@@ -31,3 +29,5 @@ require("./routes/route1.js")(app);
 app.listen(3000,()=>{
     console.log("server connected succesfully");
 })
+}
+fun();
